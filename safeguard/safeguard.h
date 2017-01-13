4e519969980a0850a7b50323ddda88d3f40e84bf
Dan <daniel.h.loman@gmail.com>
2017-01-12 18:04:09 -0800

    Initial Commit

---
+++ safeguard/safeguard.h
@@ -0,0 +1,19 @@
+//
+//  safeguard.h
+//  safeguard
+//
+//  Created by Dan Loman on 1/12/17.
+//  Copyright © 2017 Dan Loman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for safeguard.
+FOUNDATION_EXPORT double safeguardVersionNumber;
+
+//! Project version string for safeguard.
+FOUNDATION_EXPORT const unsigned char safeguardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <safeguard/PublicHeader.h>
+
+
